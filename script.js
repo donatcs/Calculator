5b1f8087354c734365b433df46a9c795f50cca22
donatcs <96204719+donatcs@users.noreply.github.com>
2022-01-23 22:07:56 +0100

    Minor tweaks and finalizing the style

--- script.js
+++ script.js
@@ -2,25 +2,20 @@ const display = document.querySelector(".display");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 
-
-
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
-
-
-
 
 numbers.forEach((numBtn) => {
   numBtn.addEventListener("click", function (event) {
     if (operator === "") {
       firstNumber += event.target.innerText;
-      console.log(parseInt(firstNumber))
+      console.log(parseInt(firstNumber));
       display.textContent = parseInt(firstNumber);
     } else {
       secondNumber += event.target.innerText;
-      console.log(parseInt(event.target.innerText))
-      display.textContent = (Number.parseInt(secondNumber))
+      console.log(parseInt(event.target.innerText));
+      display.textContent = Number.parseInt(secondNumber);
     }
   });
 });
@@ -28,7 +23,8 @@ numbers.forEach((numBtn) => {
 const inputNumber = (number) => {
   const screenNumber = Number.parseInt(display.textContent);
   if (screenNumber.length < 16)
-    display.textContent = (Number.parseInt(screenNumber) + (Number.parseInt(number)))
+    display.textContent =
+      Number.parseInt(screenNumber) + Number.parseInt(number);
 };
 
 const inputOperator = (operator) => {
@@ -37,75 +33,68 @@ const inputOperator = (operator) => {
     display.textContent = screenOperator + operator;
 };
 
-
-
 operators.forEach((opBtn) => {
   opBtn.addEventListener("click", function (event) {
     if (event.target.innerText !== "=") {
       operator = event.target.innerText;
-
-
     } else {
-      
       switch (operator) {
         case "+":
-        lovely = (parseInt(firstNumber) + parseInt(secondNumber));
-        firstNumber = lovely;  
-        secondNumber = "";
-        operator = ""
-        display.textContent = lovely; 
-
+          lastValue = parseInt(firstNumber) + parseInt(secondNumber);
+          firstNumber = lastValue;
+          secondNumber = "";
+          operator = "";
+          display.textContent = lastValue;
           break;
 
         case "-":
-          lovely = (parseInt(firstNumber) - parseInt(secondNumber));
-          firstNumber = lovely;  
+          lovely = parseInt(firstNumber) - parseInt(secondNumber);
+          firstNumber = lastValue;
           secondNumber = "";
-          operator = ""
-          display.textContent = lovely;  
+          operator = "";
+          display.textContent = lastValue;
           break;
 
         case "*":
-          lovely = (parseInt(firstNumber) * parseInt(secondNumber));
-          firstNumber = lovely;  
+          lastValue = parseInt(firstNumber) * parseInt(secondNumber);
+          firstNumber = lastValue;
           secondNumber = "";
-          operator = ""
-          display.textContent = lovely; 
+          operator = "";
+          display.textContent = lastValue;
           break;
 
         case "/":
-          lovely = (parseInt(firstNumber) / parseInt(secondNumber));
-          firstNumber = lovely;  
+          lastValue = parseInt(firstNumber) / parseInt(secondNumber);
+          firstNumber = lastValue;
           secondNumber = "";
-          operator = ""
-          display.textContent = lovely; 
+          operator = "";
+          display.textContent = lastValue;
           break;
 
-          default:
-           
+        default:
           break;
       }
     }
   });
 });
 
+let clearBtn = document.getElementById("AC");
 
-let clearBtn = document.getElementById("DEL")
+clearBtn.addEventListener("click", () => {
+  firstNumber = "";
+  secondNumber = "";
+  operator = "";
+  display.textContent = "0";
+});
 
-clearBtn.addEventListener('click', ()=> {
-    firstNumber = ""
-    secondNumber = ""
-    operator = ""
-    display.textContent = "0";
-})
+let resetBtn = document.getElementById("DEL");
 
-let resetBtn = document.getElementById("AC")
-
-resetBtn.addEventListener('click', ()=> {
-  firstNumber = firstNumber.substring(0, firstNumber.length*1-1)
-  secondNumber = secondNumber.substring(0, secondNumber.length*1-1)
-  operator = operator.substring(0, operator.length*1-1) 
-  display.textContent = display.textContent.substring(0, display.textContent.length*1-1)
-})
-
-
+resetBtn.addEventListener("click", () => {
+  firstNumber = firstNumber.substring(0, firstNumber.length * 1 - 1);
+  secondNumber = secondNumber.substring(0, secondNumber.length * 1 - 1);
+  operator = operator.substring(0, operator.length * 1 - 1);
+  display.textContent = display.textContent.substring(
+    0,
+    display.textContent.length * 1 - 1
+  );
+});
