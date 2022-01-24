33487e608db04305277d87d3f7f0c581fdc9f36d
donatcs <96204719+donatcs@users.noreply.github.com>
2022-01-24 20:23:04 +0100

    Fixed and tweaked a few things

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const display = document.querySelector(".display");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 
-let firstNumber = "";
-let secondNumber = "";
+let firstNumber = Number("");
+let secondNumber = Number("");
 let operator = "";
+let lastValue = "";
+
 
 numbers.forEach((numBtn) => {
   numBtn.addEventListener("click", function (event) {
@@ -20,64 +22,62 @@ numbers.forEach((numBtn) => {
   });
 });
 
-const inputNumber = (number) => {
-  const screenNumber = Number.parseInt(display.textContent);
-  if (screenNumber.length < 16)
-    display.textContent =
-      Number.parseInt(screenNumber) + Number.parseInt(number);
-};
-
-const inputOperator = (operator) => {
-  const screenOperator = display.textContent;
-  if (screenOperator.length < 16)
-    display.textContent = screenOperator + operator;
-};
 
 operators.forEach((opBtn) => {
   opBtn.addEventListener("click", function (event) {
     if (event.target.innerText !== "=") {
-      operator = event.target.innerText;
+      if (operator === ""){
+        operator = event.target.innerText;
+      } else { 
+        runCalculation();
+        operator = event.target.innerText;
+      }
     } else {
-      switch (operator) {
+      runCalculation()
+    }
+  });
+});
+
+function runCalculation(){ 
+  switch (operator) {
         case "+":
           lastValue = parseInt(firstNumber) + parseInt(secondNumber);
           firstNumber = lastValue;
           secondNumber = "";
-          operator = "";
           display.textContent = lastValue;
           break;
 
+
         case "-":
-          lovely = parseInt(firstNumber) - parseInt(secondNumber);
+          lastValue = parseInt(firstNumber) - parseInt(secondNumber);
           firstNumber = lastValue;
           secondNumber = "";
-          operator = "";
           display.textContent = lastValue;
           break;
+
 
         case "*":
           lastValue = parseInt(firstNumber) * parseInt(secondNumber);
           firstNumber = lastValue;
           secondNumber = "";
-          operator = "";
           display.textContent = lastValue;
           break;
+
 
         case "/":
           lastValue = parseInt(firstNumber) / parseInt(secondNumber);
           firstNumber = lastValue;
           secondNumber = "";
-          operator = "";
           display.textContent = lastValue;
           break;
-
+          
         default:
           break;
       }
-    }
-  });
-});
+      operator = "";
+}
 
+///
 let clearBtn = document.getElementById("AC");
 
 clearBtn.addEventListener("click", () => {
@@ -87,14 +87,14 @@ clearBtn.addEventListener("click", () => {
   display.textContent = "0";
 });
 
+
+///
 let resetBtn = document.getElementById("DEL");
 
 resetBtn.addEventListener("click", () => {
-  firstNumber = firstNumber.substring(0, firstNumber.length * 1 - 1);
-  secondNumber = secondNumber.substring(0, secondNumber.length * 1 - 1);
-  operator = operator.substring(0, operator.length * 1 - 1);
-  display.textContent = display.textContent.substring(
-    0,
-    display.textContent.length * 1 - 1
-  );
+
+  firstNumber = firstNumber.slice(0, -1);
+
+  display.textContent = display.textContent.slice(0, -1);
+
 });
